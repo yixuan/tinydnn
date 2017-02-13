f8a4f9e5c149da946bf6747de1757d0d80292712
Yixuan Qiu <yixuan.qiu@cos.name>
2017-02-12 19:03:47 -0500

    simple functions

--- src/tinydnn.cpp
+++ src/tinydnn.cpp
@@ -2,11 +2,23 @@
 #include <Rcpp.h>
 
 // [[Rcpp::export]]
-Rcpp::XPtr< tiny_dnn::network<tiny_dnn::sequential> > net_seq()
+Rcpp::XPtr< tiny_dnn::network<tiny_dnn::sequential> > net_seq_constructor(std::string name)
 {
     using namespace tiny_dnn;
 
-    network<sequential>* net = new network<sequential>();
+    network<sequential>* net = new network<sequential>(name);
 
     return Rcpp::XPtr< network<sequential> >(net);
+}
+
+// [[Rcpp::export]]
+std::string net_seq_name(Rcpp::XPtr< tiny_dnn::network<tiny_dnn::sequential> > net)
+{
+    return net->name();
+}
+
+// [[Rcpp::export]]
+int net_seq_layer_size(Rcpp::XPtr< tiny_dnn::network<tiny_dnn::sequential> > net)
+{
+    return net->layer_size();
 }
